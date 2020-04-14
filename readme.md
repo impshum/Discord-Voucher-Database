0f23956de9d6f0dc08d0d598b3980ad9354b4382
impshum <impshum@impshum.co.uk>
2020-04-14 15:25:20 +0100

    up we go

---
+++ readme.md
@@ -0,0 +1,15 @@
+## Discord Cinema Voucher Database
+
+Stores and supplies stored cinema vouchers over Discord.
+
+### Instructions
+
+-   Install requirements `pip install -r requirements.txt`
+-   Create Discord app and bot: https://discordpy.readthedocs.io/en/latest/discord.html
+-   Edit conf.ini with your Discord token
+-   Put all the voucher codes in their relative txt file in the data directory
+-   Run it `python run.py`
+
+### Tip
+
+BTC - 1AYSiE7mhR9XshtS4mU2rRoAGxN8wSo4tK
---
+++ run.py
@@ -0,0 +1,146 @@
+import discord
+import asyncio
+import sqlite3
+from sqlite3 import Error
+from datetime import datetime
+import configparser
+
+config = configparser.ConfigParser()
+config.read('conf.ini')
+discord_token = config['DISCORD']['token']
+types = ['ticket', 'popcorn', 'drink']
+
+
+def db_connect():
+    try:
+        conn = sqlite3.connect('data.db')
+        create_table = """CREATE TABLE IF NOT EXISTS vouchers (
+                                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        type TEXT NOT NULL,
+                                        code TEXT NOT NULL,
+                                        date TEXT NULL
+                                        );"""
+        conn.execute(create_table)
+        return conn
+    except Error as e:
+        print(e)
+    return None
+
+
+conn = db_connect()
+
+
+def insert_row(conn, type, code):
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM vouchers WHERE type='{}' AND code='{}'".format(type, code))
+    if not len(cur.fetchall()):
+        conn.execute(
+            "INSERT INTO vouchers (type, code) VALUES (?, ?)", (type, code))
+        conn.commit()
+        print(f'Adding new {type} to db')
+    cur.close()
+
+
+def search_db(conn, type, amount):
+    print('searching db')
+    results = []
+    cur = conn.cursor()
+    if type == 'bundle':
+        if type == 'bundle':
+            for i in range(amount):
+                for type in types:
+                    cur.execute(
+                        "SELECT * FROM vouchers WHERE type='{}' AND date IS NULL LIMIT 1".format(type))
+                    row = cur.fetchone()
+                    if row:
+                        results.append({row[1]: row[2]})
+                        cur.execute("UPDATE vouchers SET date='{}' WHERE ID={}".format(
+                            datetime.now(), row[0]))
+                        conn.commit()
+    else:
+        cur.execute(
+            "SELECT * FROM vouchers WHERE type='{}' AND date IS NULL LIMIT {}".format(type, amount))
+        rows = cur.fetchall()
+        for row in rows:
+            results.append({row[1]: row[2]})
+            cur.execute("UPDATE vouchers SET date='{}' WHERE ID={}".format(
+                datetime.now(), row[0]))
+            conn.commit()
+    cur.close()
+    return results
+
+
+def read_db(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM vouchers")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+def populate_db(conn):
+    print('populating db')
+    for type in types:
+        with open(f'data/{type}.txt') as f:
+            for code in f.read().splitlines():
+                insert_row(conn, type, code)
+
+
+class DiscordClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user.name}')
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+
+        user_input = message.content
+
+        if user_input.startswith('/v') and user_input != '/v':
+            populate_db(conn)
+            order = {}
+
+            codes = user_input.replace('/v', '').strip().split(' ')
+            print(' '.join(codes))
+            for code in codes:
+                if code.endswith('b'):
+                    amount = code.replace('b', '')
+                    order['bundle'] = amount
+                elif code.endswith('t'):
+                    amount = code.replace('t', '')
+                    order['ticket'] = amount
+                elif code.endswith('p'):
+                    amount = code.replace('p', '')
+                    order['popcorn'] = amount
+                elif code.endswith('d'):
+                    amount = code.replace('d', '')
+                    order['drink'] = amount
+
+            reply = ''
+            print(order)
+            for item, amount in order.items():
+                results = search_db(conn, item, int(amount))
+                for result in results:
+                    for k, v in result.items():
+                        reply += f'{k} - {v}\n'
+
+            if not len(reply):
+                reply = 'No vouchers left!'
+            return await message.channel.send(reply)
+
+        elif user_input == '/help':
+            print('help')
+            reply = '/help - show help\n/readdb - read all db\n\nstart the command with /v\n\n1b = 1 x bundle\n1t = 1 x ticket\n1p = 1 x popcorn\n1d = 1 x drink\n\nExample\n/v 2t 1p 2d'
+            return await message.channel.send(reply)
+
+
+def main():
+    client = DiscordClient()
+    client.run(discord_token)
+
+
+if __name__ == '__main__':
+    main()
